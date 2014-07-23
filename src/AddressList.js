1bce00635c79d656b3ff173d6405ccab234e266f
Al Whiting <alwhiting@gmail.com>
2014-07-22 21:08:40 -0600

    Removed simplistic filtering from AddressList; the API can filter the bad addresses for us!

--- src/AddressList.js
+++ src/AddressList.js
@@ -8,12 +8,6 @@ function AddressList(addressList) {
             // some entries have quotes; remove them, clear up any excess whitespace
             .map(function (address) {
                 return address.replace(/\"/g, '').trim();
-            })
-            // the address list contains 'Address' as the first line, I felt like it
-            // would be cheating to just remove that so this filter is used to make
-            // a quasi-intelligent effort to remove non-address elements
-            .filter(function (address) {
-                return address.length > 0 && !isNaN(parseInt(address[0], 10));
             });
 
     } else {
